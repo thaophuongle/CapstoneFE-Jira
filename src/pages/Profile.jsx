9aaa5972c06a8f166a2dd69f0d4a691764326a3b
thaophuongle <53205273+thaophuongle@users.noreply.github.com>
2024-05-25 15:05:13 +1000

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,63 @@
-import React, { useEffect, useState } from "react";
-import * as yup from "yup";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-
+import { getAllProjectApiAction } from "../redux/Reducers/HomeReducer";
+import { Table, Avatar, Tooltip } from "antd";
 
 const Profile = () => {
-  const { userLogin } = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
 
+  const { userLogin } = useSelector((state) => state.userReducer);
+  const { arrData } = useSelector((state) => state.homeReducer);
 
+  useEffect(() => {
+    dispatch(getAllProjectApiAction());
+  }, [dispatch]);
 
+  useEffect(() => {
+    //console.log("userLogin", userLogin);
+    //console.log("arrData", arrData);
+  }, [arrData, userLogin]);
+
+  const filteredProjects = arrData.filter(project => project.creator.id === userLogin.id);
+
+  console.log("Filtered Projects", filteredProjects); 
+
+  const columns = [
+    {
+      title: 'Project ID',
+      dataIndex: 'id',
+      key: 'id',
+      width: '10%',
+    },
+    {
+      title: 'Project Name',
+      dataIndex: 'projectName',
+      key: 'projectName',
+      render: (text, record) => (
+        <NavLink to={`/projects/projectDetail/${record.id}`}>
+          {text}
+        </NavLink>
+      ),
+    },
+    {
+      title: 'Members',
+      dataIndex: 'members',
+      key: 'members',
+      render: (members) => (
+        <Avatar.Group
+          maxCount={2}
+          maxStyle={{ color: '#f56a00', backgroundColor: '#fde3cf' }}
+        >
+          {members.map((member, index) => (
+            <Tooltip title={member.name} key={index} placement="top">
+              <Avatar src={member.avatar} alt={member.name} />
+            </Tooltip>
+          ))}
+        </Avatar.Group>
+      ),
+    },
+  ];
 
   return (
     <div className="container py-4 px-5">
@@ -42,6 +90,11 @@ const Profile = () => {
             <NavLink to={"/projects/update-profile"} className={"btn btn-primary"}>Update Profile</NavLink>
           </div>
         </div>
+      </div>
+
+      <div className="mt-5">
+        <h5 className="pb-4">My Projects</h5>
+        <Table columns={columns} dataSource={filteredProjects} rowKey="id" />
       </div>
     </div>
   );
